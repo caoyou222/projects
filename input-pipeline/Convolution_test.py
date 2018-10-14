0b53b02177963905e825cb86e055d8221a8ba2ff
CollinScott22 <42358381+CollinScott22@users.noreply.github.com>
2018-10-14 17:23:04 -0500

    I added an example of what the model could look like

--- input-pipeline/Convolution_test.py
+++ input-pipeline/Convolution_test.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.python.keras.datasets import mnist
-from tensorflow.python.keras.layers import Dense, Flatten
+from tensorflow.python.keras.layers import Dense, Flatten, Dropout
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.python.keras.models import Sequential
 
@@ -22,5 +22,19 @@ x_test  = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
 
-
+"""
 model = Sequential()
+model.add(Conv2D(32, kernel_size = (5, 5),
+                 strides = (1, 1)
+                 activation = 'relu'
+                 input_shape = input_shape))
+model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+model.add(Conv2D(kernel_size = (5, 5),
+                 strides = (1, 1)
+                  activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2). strides = (2, 2)))
+model.add(Flatten())
+model.add(Dense())
+model.add(Flatten())
+model.add(Dense())
+"""
