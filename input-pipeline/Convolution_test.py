c741cb715dacb1d35b29cf630c0cb1867cb33b61
CollinScott22 <42358381+CollinScott22@users.noreply.github.com>
2018-10-14 16:56:23 -0500

    edited the input file lines

--- input-pipeline/Convolution_test.py
+++ input-pipeline/Convolution_test.py
@@ -1,27 +1,22 @@
-import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from Keras.layers import Conv2D, MaxPooling2D
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.python.keras.datasets import mnist
+from tensorflow.python.keras.layers import Dense, Flatten
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.python.keras.models import Sequential
 
-batch_size = 130
-num_classes = 10
-epochs = 12
 
-#inputting image dimensions
+batch_size = 100
+epoch = 12
 
-img_rows, img_columns = 28,28
+img_x, img_y = 28, 28
 
-(x_train,y_train), (x_test,y_test) = mnist.load_data()
+(x_train,y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train.reshape(x_train.shape[0], img_x, img_y, 1)
 x_test = x_test.reshape(x_test.shape[0], img_x, img_y, 1)
-input_shape(img_x, img_y, 1)
+input_shape = (img_x, img_y, 1)
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
 
-##x_train /= 255
-##x_test /= 255
-
-model = Sequential()
+x_train /= 255
+x_test /= 255
