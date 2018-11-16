085c1f12ed25b313ec34e3684d0d9b0a52bc186d
caoyou222 <cao1@stolaf.edu>
2018-11-15 23:38:15 -0600

    newest version of lenet5

--- Desktop/Capstone/CNN/tower-recog/lenet5.py
+++ Desktop/Capstone/CNN/tower-recog/lenet5.py
@@ -3,12 +3,15 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt 
 import time
+from PIL import Image 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 keep_prob=tf.placeholder("float")
-learning_rate = 0.001
+learning_rate = 0.0001
+batch_size = 50
+class_num = 3
 
-def read_and_decode(filename):
+def read_and_decode(filename, b_size):
     filename_queue = tf.train.string_input_producer([filename])
  
     reader = tf.TFRecordReader()
@@ -20,11 +23,13 @@ def read_and_decode(filename):
                                        })
  
     img = tf.decode_raw(features['img_raw'], tf.uint8)
-    img = tf.reshape(img, [4096])
-    img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
+    img = tf.reshape(img, [1024])
     label = tf.cast(features['label'], tf.int32)
 
-    return img, label
+    img_train_batch, labels_train_batch = tf.train.shuffle_batch([img, label],batch_size=b_size,capacity=10000,min_after_dequeue=9000,num_threads=2)
+    labels_batch = tf.one_hot(labels_train_batch,depth=class_num)
+
+    return img_train_batch, labels_batch
 
 def weight_variable(shape):
     initial=tf.truncated_normal(shape,stddev=0.1)
@@ -37,8 +42,7 @@ def bias_variable(shape):
 # convolution and pooling
 def conv2d(x,W,b):
     h_conv = tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='VALID')
-    conv = tf.nn.tanh(tf.nn.bias_add(h_conv,b))
-    return tf.layers.batch_normalization(conv, training=True)
+    return tf.nn.relu(tf.nn.bias_add(h_conv,b))
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
@@ -47,31 +51,26 @@ def max_pool_2x2(x):
 def lenet5_layer(layer,weight,bias):
     W_conv=weight_variable(weight)
     b_conv=bias_variable(bias)
+
     h_conv=conv2d(layer,W_conv,b_conv)
     return max_pool_2x2(h_conv)
 
 # connected layer
 def dense_layer(layer,weight,bias):
+    W_fc=weight_variable(weight)
     b_fc=bias_variable(bias)
-    return tf.nn.tanh(tf.matmul(layer,weight)+b_fc)
+
+    return tf.matmul(layer,W_fc)+b_fc
 
 def main():
     time_start = time.time()
-    image, label = read_and_decode("tower.tfrecords")
-    img_train_batch, labels_train_batch = tf.train.shuffle_batch([image, label],batch_size=20,capacity=15000,min_after_dequeue=6000,num_threads=2)
-    labels_train_batch = tf.one_hot(labels_train_batch,depth=3) #resize the label into an array
-    label = tf.one_hot(label, depth=3)
-    sess=tf.InteractiveSession()
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    regularizer = tf.contrib.layers.l2_regularizer(0.001)
+    img_train_batch, labels_train_batch = read_and_decode('tower_train.tfrecords', batch_size)
 
-    # input layer
-    x=tf.placeholder("float",shape=[None,4096])
-    y_=tf.placeholder("float",shape=[None,3])
+    x=tf.placeholder("float",shape=[None,1024])
+    y_=tf.placeholder("float",shape=[None,class_num])
 
     # first layer
-    x_image=tf.reshape(x,[-1,64,64,1])
+    x_image=tf.reshape(x,[-1,32,32,1])
     layer=lenet5_layer(x_image,[5,5,1,6],[6])
 
     # second layer
@@ -80,23 +79,16 @@ def main():
     # third layer
     W_conv3=weight_variable([5,5,16,120])
     b_conv3=bias_variable([120])
-    if regularizer != None:
-            tf.add_to_collection('losses',regularizer(W_conv3))
+
     layer=conv2d(layer,W_conv3,b_conv3)
-    layer = tf.reshape(layer,[-1,9*9*120])
+    layer=tf.reshape(layer,[-1,120])
 
     # all connected layer
-    w_fc1 = weight_variable([9*9*120,84])
-    if regularizer != None:
-            tf.add_to_collection('losses',regularizer(w_fc1))
-    con_layer=dense_layer(layer,w_fc1,[84])
+    con_layer=dense_layer(layer,[120,84],[84])
 
     # output
-    w_fc2 = weight_variable([84,3])
-    if regularizer != None:
-            tf.add_to_collection('losses',regularizer(w_fc2))
-    con_layer=dense_layer(con_layer,w_fc2,[3])
-    y_conv=tf.nn.softmax(tf.nn.dropout(con_layer,0.5))
+    con_layer=dense_layer(con_layer,[84,class_num],[class_num])
+    y_conv=tf.nn.softmax(tf.nn.dropout(con_layer,keep_prob))
 
     # train and evalute
     cross_entropy=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_,logits=y_conv))
@@ -105,31 +97,43 @@ def main():
     accuracy=tf.reduce_mean(tf.cast(correct_prediction,"float"))
 
     saver = tf.train.Saver()
-
-    sess.run(tf.global_variables_initializer())
-
-    try:
-        accr = []
-        costs = []
-        for i in range(10000):
-            img_xs, label_xs = sess.run([img_train_batch, labels_train_batch])
+    accr = []
+    costs = []
+    with tf.Session() as sess:
+        
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        for i in range(2000):
+            img_xs, label_xs = sess.run([img_train_batch,labels_train_batch])
             if i%100==0:
                 train_accuracy=accuracy.eval(feed_dict={x:img_xs,y_:label_xs,keep_prob:1.0})
-                batch_cost = cross_entropy.eval(feed_dict={x:img_xs,y_:label_xs,keep_prob:1.0})
                 accr.append(train_accuracy)
+                batch_cost = cross_entropy.eval(feed_dict={x:img_xs,y_:label_xs,keep_prob:1.0})
                 costs.append(batch_cost)
-                print("step %d,training accuracy %g"%(i,train_accuracy))
-                print("step %d,cost %g"%(i,batch_cost))
+                print("step %d, training accuracy %g"%(i,train_accuracy) + ", cost %g"%(batch_cost))
             train_step.run(feed_dict={x:img_xs,y_:label_xs,keep_prob:0.5})
-    except Exception, e:
-        coord.request_stop(e)
-    coord.request_stop()
-    coord.join(threads)
-    saver.save(sess,"./model.ckpt")
-    sess.close()
+        
+        coord.request_stop()
+        coord.join(threads)
 
-    time_stop = time.time()
-    print("Training Time: " + str(time_stop-time_start) + "s")
+        saver.save(sess,"./tower_model.ckpt")
+
+    # Test
+    # img_test_batch, labels_test_batch = read_and_decode('tower_test.tfrecords',500)
+    # with tf.Session() as sess:
+    #     saver.restore(sess, 'tower_model.ckpt')
+    #     coord = tf.train.Coordinator()
+    #     threads = tf.train.start_queue_runners(coord=coord)
+    #     for i in range(10):
+    #         img_xs, label_xs = sess.run([img_test_batch,labels_test_batch])
+    #         test_accuracy=accuracy.eval(feed_dict={x:img_xs,y_:label_xs,keep_prob:1.0})
+    #         batch_cost = cross_entropy.eval(feed_dict={x:img_xs,y_:label_xs,keep_prob:1.0})
+    #         print("step %d, testing accuracy %g"%(i,test_accuracy) + ", cost %g"%(batch_cost))
+        
+    #     coord.request_stop()
+    #     coord.join(threads)
 
     plt.plot(accr)
     plt.ylabel('accuracy')
@@ -142,6 +146,10 @@ def main():
     plt.xlabel('iterations (per 100)')
     plt.title("Learning rate =" + str(learning_rate))
     plt.show()
+
+    time_stop = time.time()
+    print("Training Time: " + str(time_stop-time_start) + "s")
+
 
 if __name__=='__main__':
     main()
